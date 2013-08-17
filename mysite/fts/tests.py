9785611377ff0dea9415e18dfff01478415c75dd
Daley Chetwynd <d_chetwynd@fastmail.co.uk>
2013-08-17 16:46:52 +0100

    Added choices into polls on admin page

--- mysite/fts/tests.py
+++ mysite/fts/tests.py
@@ -1,7 +1,19 @@
+from collections import namedtuple
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+
+PollInfo = namedtuple('PollInfo', ['question', 'choices'])
+
+POLL1 = PollInfo(
+    question = "How awesome is Test-Driven Development?",
+    choices = [
+        'Very awesome',
+	'Quite awesome',
+	'Moderately awesome',
+    ],
+)
 
 class AdminSiteTest(LiveServerTestCase):
     fixtures = ['admin_user.json']
@@ -32,3 +44,71 @@ class AdminSiteTest(LiveServerTestCase):
 
         polls_links = self.browser.find_elements_by_link_text('Polls')
         self.assertEquals(len(polls_links), 2)
+
+	polls_links[1].click()
+
+	body = self.browser.find_element_by_tag_name('body')
+	self.assertIn('0 polls', body.text)
+	
+	new_poll_link = self.browser.find_element_by_link_text('Add poll')
+	new_poll_link.click()
+
+	body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Question:', body.text)
+	self.assertIn('Date published:', body.text)
+	
+	question_field = self.browser.find_element_by_name('question')
+	question_field.send_keys("How awesome is Test-Driven Development?")
+
+	date_field = self.browser.find_element_by_name('pub_date_0')
+	date_field.send_keys('01/01/12')
+	time_field = self.browser.find_element_by_name('pub_date_1')
+	time_field.send_keys('00:00')
+
+	choice_1 = self.browser.find_element_by_name('choice_set-0-choice')
+	choice_1.send_keys('Very awesome')
+	choice_2 = self.browser.find_element_by_name('choice_set-1-choice')
+	choice_2.send_keys('Quite awesome')
+	choice_3 = self.browser.find_element_by_name('choice_set-2-choice')
+	choice_3.send_keys('Moderately awesome')
+
+        save_button = self.browser.find_element_by_css_selector("input[value='Save']")
+	save_button.click()
+
+	new_poll_links = self.browser.find_elements_by_link_text(
+	    "How awesome is Test-Driven Development?"
+	)
+	self.assertEquals(len(new_poll_links), 1)
+
+    def _setup_polls_via_admin(self):
+        self.browser.get(self.live_server_url + '/admin')
+	username_field = self.browser.find_element_by_name('username')
+	username_field.send_keys('admin')
+	password_field = self.browser.find_element_by_name('password')
+	password_field.send_keys('admin')
+	password_field.send_keys(Keys.RETURN)
+
+	for poll_info in [POLL1]:
+	    self.browser.find_elements_by_link_text('Polls')[1].click()
+	    self.browser.find_elements_by_link_text('Add poll').click()
+
+	    question_field = self.browser.find_element_by_link_text('Today').click()
+	    question_field.send_keys(poll_info.question)
+	    self.browser.find_element_by_link_text('Today').click()
+	    self.browser.find_element_by_link_text('Now').click()
+
+	    for i, choice_text in enumerate(poll_info.choices):
+	        choice_field = self.browser.find_element_by_name('choice_set-%d-choice' % i)
+		choice_field.send_keys(choice_text)
+
+            save_button = self.browser.find_element_by_css_selector("input[value='Save']")
+	    save_button.click()
+
+	    new_poll_links = self.browser.find_elements_by_link_text(
+	        poll_info.question
+	    )
+	    self.assertEquals(len(new_poll_links), 1)
+
+	    self.browser.get(self.live_server_url + '/admin/')
+	
+	self.browser.find_element_by_link_text('Log out').click()
--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.utils import timezone
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 class PollModelTest(TestCase):
     def test_creating_a_new_poll_and_saving_it_to_the_database(self):
@@ -17,3 +17,34 @@ class PollModelTest(TestCase):
 
         self.assertEquals("What's up?", only_poll_in_database.question)
         self.assertEquals(poll.pub_date, only_poll_in_database.pub_date)
+
+    def test_verbose_name_for_pub_date(self):
+        for field in Poll._meta.fields:
+	    if field.name == 'pub_date':
+	        self.assertEquals('Date published', field.verbose_name)
+
+    def test_poll_objects_are_named_after_their_question(self):
+        p = Poll()
+	p.question = "How is babby formed?"
+	self.assertEquals("How is babby formed?", unicode(p))
+	
+class ChoiceModelTest(TestCase):
+    def test_creating_some_choices_for_a_poll(self):
+        poll = Poll()
+	poll.question = "What's up?"
+	poll.pub_date = timezone.now()
+	poll.save()
+
+	choice = Choice()
+	choice.poll = poll
+	choice.choice = "doin' fine..."
+	choice.votes = 3
+	choice.save()
+
+	poll_choices = poll.choice_set.all()
+	self.assertEquals(1, poll_choices.count())
+
+	choice_from_db = poll_choices[0]
+	self.assertEquals(choice, choice_from_db)
+	self.assertEquals("doin' fine...", choice_from_db.choice)
+	self.assertEquals(3, choice_from_db.votes)
